ed537ca334195c434f408ecfc1f28f76e2ea4744
wajihMahrouseh <wajiho.mahrouseh@gmail.com>
2024-02-03 12:24:53 +0300

    building wrapper components

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -2,17 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 import AllMeetupPage from './pages/AllMeetup';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
-import MainNavigation from './components/layouts/MainNavigation';
+import Layout from './components/layouts/Layout';
 
 function App() {
   return (
     <div className="App">
-      <MainNavigation />
-      <Routes>
-        <Route path="/" element={<AllMeetupPage />} />
-        <Route path="/new-meetup" element={<NewMeetupPage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<AllMeetupPage />} />
+          <Route path="/new-meetup" element={<NewMeetupPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+        </Routes>
+      </Layout>
     </div >
   );
 }
