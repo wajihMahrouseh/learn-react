7345f896b6916f8523257787d5aee74ec790f79a
wajihMahrouseh <wajiho.mahrouseh@gmail.com>
2024-02-03 08:13:34 +0300

    first app

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -4,7 +4,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>Learn React</h1>
+        <div>
+          <h2>My Todos</h2>
+          <div>
+            <h3>TITLE</h3>
+            <div>
+              <button>Delete</button>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
