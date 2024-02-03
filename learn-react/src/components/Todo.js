ef26bb6448c6f99521f442b423143faf8cf759d3
wajihMahrouseh <wajiho.mahrouseh@gmail.com>
2024-02-03 09:28:19 +0300

    handling events

--- learn-react/src/components/Todo.js
+++ learn-react/src/components/Todo.js
@@ -1,11 +1,17 @@
 function Todo(props) {
+    function deleteHandler() {
+        console.log('Clicked!')
+    }
+
     return (
         <div>
             <h3>{props.text}</h3>
             <div>
-                <button>Delete</button>
+                <button onClick={deleteHandler}>Delete</button>
+                {/* <button onClick={() => { }}>Delete</button> */}
+                {/* <button onClick={function () { }}>Delete</button> */}
             </div>
-        </div>
+        </div >
     );
 }
 
